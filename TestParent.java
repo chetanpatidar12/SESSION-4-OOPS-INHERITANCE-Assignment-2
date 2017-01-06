11e901f7cbda0b7d1b2c627f3b2dd9edd01c93ce
chetan patidar <chetan patidar>
2017-01-07 03:41:58 +0530

    Signed-off-by: chetan patidar <chetan patidar>

---
+++ TestParent.java
@@ -0,0 +1,10 @@
+package com.acad.basic;
+
+public class TestParent {
+	public static void main(String args[]) {
+		Parent p = new Child();
+		p.method2();
+}
+//ANSWER-C. Prints : Parent's method2()
+ Parent's method1()
+}
